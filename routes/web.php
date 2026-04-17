99edb1fb4838bf5e875657bdbb06623f60f0ae43
thanyow <maurithania.tjakra@gmail.com>
2026-04-17 23:04:29 +0700

    RC-2: Implement Admin logic to define and manage CO2 constants

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\CO2Controller;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -11,12 +12,16 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-
+// [PBI-01] Admin Routes to define and manage CO2 constants
+Route::post('/admin/categories', [CO2Controller::class, 'addCategory']);
+Route::put('/admin/categories/{id}/co2-constant', [CO2Controller::class, 'updateCategoryCO2']);
+Route::delete('/admin/categories/{id}', [CO2Controller::class, 'deleteCategory']);
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
 
 require __DIR__.'/auth.php';
