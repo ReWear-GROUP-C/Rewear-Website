4b17c276ac0bb095bb70ac02d5526bdb7eec4f17
benzzco <benaziccodaffa54321@gmail.com>
2026-04-21 21:42:59 +0700

    RC-12: Add controller for the post feature

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/community', [PostController::class, 'index'])->name('community.index');
+Route::post('/community/create', [PostController::class, 'store'])->name('community.store');
+Route::put('/community/update/{id}', [PostController::class, 'update'])->name('community.update');
+Route::delete('/community/delete/{id}', [PostController::class, 'destroy'])->name('community.destroy');
